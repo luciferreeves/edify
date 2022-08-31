194f669897b4b242c2996bed375352abdbefaa31
Bobby <bobbyskhs@gmail.com>
2022-08-30 23:06:15 -0400

    Added Tests for Validating Emails

---
+++ tests/test_email.py
@@ -0,0 +1,56 @@
+from edify.library import email
+
+
+def test():
+    emails = [
+        "email@example.com",
+        "email@192.168.0.1",
+        "firstname.lastname@example.com",
+        "email@subdomain.example.com",
+        "firstname+lastname@example.com",
+        "1234567890@example.com",
+        "email@example-one.com",
+        "_______@example.com",
+        "email@example.museum",
+        "firstname-lastname@example.com",
+        "email@example.com.",
+        "plainaddress",
+        "#@%^%#$@#$@#.com",
+        "@example.com",
+        "Joe Smith <email@example.com>",
+        "email.example.com",
+        "email@example@example.com",
+        ".email@example.com",
+        "email.@example.com",
+        "email..email@example.com",
+        "あいうえお@example.com",
+        "email@-example.com",
+        "Abc..123@example.com"
+    ]
+    expectations = [
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        True,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False
+    ]
+    for i in range(len(emails)):
+        assert email(emails[i]) == expectations[i]
